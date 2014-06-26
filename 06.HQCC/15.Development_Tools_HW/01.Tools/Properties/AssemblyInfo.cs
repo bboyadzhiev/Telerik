90cd6d6b2946365d1e366c39e054d9152e7f07f9
bboyadzhiev <bboyadzhiev@gmail.com>
2014-06-26 23:49:07 +0300

    Development Tools Homework

--- 06.HQCC/15.Development_Tools_HW/01.Tools/Properties/AssemblyInfo.cs
+++ 06.HQCC/15.Development_Tools_HW/01.Tools/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("16b29eec-a42f-400e-a5bd-914ed47c8b4a")]
+[assembly: Guid("91357321-6372-4c5a-9898-1486727feae2")]
 
 // Version information for an assembly consists of the following four values:
 //
