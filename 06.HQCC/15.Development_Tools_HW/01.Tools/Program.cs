9c98405905854299637aff53e2537b716962d28d
bboyadzhiev <bboyadzhiev@gmail.com>
2014-06-27 11:51:44 +0300

    homework

--- 06.HQCC/15.Development_Tools_HW/01.Tools/Program.cs
+++ 06.HQCC/15.Development_Tools_HW/01.Tools/Program.cs
@@ -1,15 +1,89 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace _01.Tools
+﻿namespace Tools
 {
-    class Program
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// Main program
+    /// </summary>
+    public class Program
     {
-        static void Main(string[] args)
+        /// <summary>
+        /// Prints a menu to the Console
+        /// </summary>
+        public static void PrintMenu()
         {
+            Console.WriteLine("Calculate area using: ");
+            Console.WriteLine("1. The length of a side and an altitude to it");
+            Console.WriteLine("2. The length of the three sides");
+            Console.WriteLine("3. The length of two of the sides and the angle between them");
+            Console.WriteLine();
+            Console.Write("Choice: ");
+        }
+
+        /// <summary>
+        /// Calculates the area of a triangle, using the method, specified by 'choice'
+        /// </summary>
+        /// <param name="choice">Specifies the method to be used to calculate the area</param>
+        /// <returns>The calculate area</returns>
+        public static double CalculateArea(byte choice)
+        {
+            double paramA = 0;
+            double paramB = 0;
+            double paramC = 0;
+            float angle = 0;
+            switch (choice)
+            {
+                case 1: Console.WriteLine("- Enter the side's length and altitude to it.");
+                    Console.Write("Side length: ");
+                    double.TryParse(Console.ReadLine(), out paramA);
+                    Console.Write("Altitude: ");
+                    double.TryParse(Console.ReadLine(), out paramB);
+                    return Triangle.GetSurface(paramA, paramB);
+                case 2: Console.WriteLine("- Ener the length of the three sides");
+                    Console.Write("First side: ");
+                    double.TryParse(Console.ReadLine(), out paramA);
+                    Console.Write("Second side: ");
+                    double.TryParse(Console.ReadLine(), out paramB);
+                    Console.Write("Third side: ");
+                    double.TryParse(Console.ReadLine(), out paramC);
+                    return Triangle.GetSurface(paramA, paramB, paramC);
+                case 3: Console.WriteLine("- Enter two sides and the angle between them");
+                    Console.Write("First side: ");
+                    double.TryParse(Console.ReadLine(), out paramA);
+                    Console.Write("Second side: ");
+                    double.TryParse(Console.ReadLine(), out paramB);
+                    Console.Write("Angle: ");
+                    float.TryParse(Console.ReadLine(), out angle);
+                    angle = (float)(Math.PI * angle / 180.0);
+                    return Triangle.GetSurface(paramA, paramB, angle);
+                default:
+                    return 0;
+            }
+        }
+
+        /// <summary>
+        /// Main entry point
+        /// </summary>
+        public static void Main()
+        {
+            Console.WriteLine("--- Calculates a triangle's area by given parameters ---");
+            PrintMenu();
+            double area = 0;
+            byte choice;
+            byte.TryParse(Console.ReadLine(), out choice);
+            if (choice != 0)
+            {
+                area = CalculateArea(choice);
+                Console.WriteLine("The area of the triangle is: {0}", area);
+            }
+            else
+            {
+                Console.WriteLine("Incorrect choice!");
+            }
         }
     }
 }
---
+++ 06.HQCC/15.Development_Tools_HW/01.Tools/Triangle.cs
@@ -0,0 +1,53 @@
+﻿namespace Tools
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// Represents a Triangle
+    /// </summary>
+    public class Triangle
+    {
+        /// <summary>
+        /// Calculates the surface of a triangle, based on 
+        /// side and the altitude to it
+        /// </summary>
+        /// <param name="side">The length of the side</param>
+        /// <param name="altitude">The length of the altitude</param>
+        /// <returns>The area of the triangle</returns>
+        public static double GetSurface(double side, double altitude)
+        {
+            return 0.5 * side * altitude;
+        }
+
+        /// <summary>
+        /// Calculates the surface of a triangle, based on
+        /// the lengths of its sides. Uses Heron's formula
+        /// </summary>
+        /// <param name="sideA">The length of the first side</param>
+        /// <param name="sideB">The length of the second side</param>
+        /// <param name="sideC">The length of the third side</param>
+        /// <returns>The surface of the triangle</returns>
+        public static double GetSurface(double sideA, double sideB, double sideC)
+        {
+            double p = (sideA + sideB + sideC) / 2;
+            return Math.Sqrt(p * (p - sideA) * (p - sideB) * (p - sideC));
+        }
+
+        /// <summary>
+        /// Calculates the surface of a triangle, based on
+        /// the lengths of two of its sides and the angle between them
+        /// </summary>
+        /// <param name="sideA">The length of the first side</param>
+        /// <param name="sideB">The length of the second side</param>
+        /// <param name="angle">The value of the angle between them</param>
+        /// <returns>The surface of the triangle</returns>
+        public static double GetSurface(double sideA, double sideB, float angle)
+        {
+            return (double)(0.5 * sideA * sideB * Math.Sin(angle));
+        }
+    }
+}
