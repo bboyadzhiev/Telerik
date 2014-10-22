a2f196d58fdf0c30ee386556357ae4d4a4822710
Borg <borg@Borgs-Mac.local>
2014-10-22 10:48:07 +0300

    Initial Commit

---
+++ 16.iOS Applications/SquareMatrix/SquareMatrix/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  SquareMatrix
+//
+//  Created by Borg on 10/22/14.
+//  Copyright (c) 2014 Borg. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
